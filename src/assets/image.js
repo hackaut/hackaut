77856bbf5773ae7b03337d55bd1f69d0c5e6e78f
Parthib Mukherjee <parthibmukherjee@gmail.com>
2024-05-05 23:33:13 +0530

    Added a image-carousel showing previous event

---
+++ src/assets/image.js
@@ -0,0 +1,35 @@
+import image1 from './event/1.jpg';
+import image2 from './event/2.jpg';
+import image3 from './event/3.jpg';
+import image4 from './event/4.jpg';
+import image5 from './event/5.jpg';
+import image6 from './event/6.jpg';
+const images = [
+    {
+        original: image1,
+        thumbnail: image1,
+      },
+      {
+        original: image2,
+        thumbnail: image2,
+      },
+      {
+        original: image3,
+        thumbnail: image3,
+      },
+      {
+        original: image4,
+        thumbnail: image4,
+      },
+      {
+        original: image5,
+        thumbnail: image5,
+      },
+      {
+        original: image6,
+        thumbnail: image6,
+      },
+];
+
+export default images;
+
