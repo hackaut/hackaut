8641e39d5469c4ef1fbfc86c971372ee6ca75dba
Arnab Pal <arnabpalofficial2003@gmail.com>
2024-05-11 13:49:08 +0530

    improved and featured lazyload

--- src/Componants/Teams/TeamCard.jsx
+++ src/Componants/Teams/TeamCard.jsx
@@ -1,22 +1,36 @@
-import { FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { IoPerson } from "react-icons/io5";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+import image from "../../assets/person.webp";
+
 export const TeamCard = (props) => {
   return (
     <div className="w-40">
-      <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
-        <img
-          className="absolute object-cover w-full h-full rounded-xl"
-          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-          alt="Person"
+      <div className="relative w-40 h-40 mb-4 rounded-[50%] shadow">
+        <LazyLoadImage
+          alt="person"
+          className="absolute object-cover w-full h-full rounded-[50%]"
+          height={160}
+          width={160}
+          effect="blur"
+          src={image}
+          
         />
+        {/* <img
+          className="absolute object-cover w-full h-full rounded-[50%]"
+          src={image}
+          alt="Person"
+        /> */}
       </div>
       <div className="flex flex-col text-center">
-        <p className="text-lg font-bold">{props.name}</p>
+        <p className="text-lg font-extrabold">{props.name}</p>
         <p className="mb-5 text-xs text-gray-800">{props.role}</p>
         <div className="flex items-center justify-center space-x-3">
           <a
             href="/"
-            className="text-gray-600 transition-colors duration-300 hover:text-blue-500"
+            className="text-gray-600 transition-colors duration-300 hover:text-blue-900"
           >
             <IoPerson size={20}></IoPerson>
           </a>
@@ -28,9 +42,9 @@ export const TeamCard = (props) => {
           </a>
           <a
             href="/"
-            className="text-gray-600 transition-colors duration-300 hover:text-fuchsia-400"
+            className="text-gray-600 transition-colors duration-300 hover:text-blue-400"
           >
-            <FaInstagram size={20}></FaInstagram>
+            <FaLinkedin size={20}></FaLinkedin>
           </a>
         </div>
       </div>
