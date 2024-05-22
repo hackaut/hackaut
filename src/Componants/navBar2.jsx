8ccb6964852815631105242ccb74ccd47351269f
RDX-Rudra <dasrudra234@gmail.com>
2024-05-23 02:12:45 +0530

    logo added in nacessary position

--- src/Componants/navBar2.jsx
+++ src/Componants/navBar2.jsx
@@ -16,10 +16,17 @@ const NavBar2 = (props) => {
   return (
     <header className="flex flex-wrap md:justify-start sm:flex-nowrap z-50 w-full bg-blue-200 text-sm py-4 border-b-2 border-blue-700 z-100 sticky top-0  ">
       <nav className="max-w-[85rem] w-full mx-auto px-4 md:flex md:items-center md:justify-between">
+        <NavLink to="/" className="flex-none text-xl font-black" style={{display: 'flex', gap: '10px'}}>
+          <img
+            className="logonav"
+            src="src/assets/Group 2.png"
+            alt="Logo"
+            style={{ width: '25px', height: 'auto' }}
+          />
+          <h2>HACKAUT</h2>
+        </NavLink>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-          <NavLink to="/" className="flex-none text-xl font-black"> {/* Use Link instead of <a> */}
-            HACKAUT
-          </NavLink>
+
 
           <div className="md:hidden">
             <button
@@ -42,7 +49,7 @@ const NavBar2 = (props) => {
               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-200 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
                 {links.map((link) => {
                   return (
-                    <li key={link.id} onClick={()=>{props.setNav(!props.nav)}}>
+                    <li key={link.id} onClick={() => { props.setNav(!props.nav) }}>
                       <NavLink
                         to={link.path} // Use Link instead of <a> and remove smooth and duration attributes
                         className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
