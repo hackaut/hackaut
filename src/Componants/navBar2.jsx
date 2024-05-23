c9e93eb418b9d19773c8e7011e38b350d745550c
RDX-Rudra <dasrudra234@gmail.com>
2024-05-23 11:57:12 +0530

    RDX

--- src/Componants/navBar2.jsx
+++ src/Componants/navBar2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom"; // Import Link from react-router-dom instead of react-scroll
+import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const links = [
@@ -14,64 +14,61 @@ const links = [
 
 const NavBar2 = (props) => {
   return (
-    <header className="flex flex-wrap md:justify-start sm:flex-nowrap z-50 w-full bg-blue-200 text-sm py-4 border-b-2 border-blue-700 z-100 sticky top-0  ">
-      <nav className="max-w-[85rem] w-full mx-auto px-4 md:flex md:items-center md:justify-between">
-        <NavLink to="/" className="flex-none text-xl font-black" style={{display: 'flex', gap: '10px'}}>
-          <img
-            className="logonav"
-            src="src/assets/Group 2.png"
-            alt="Logo"
-            style={{ width: '25px', height: 'auto' }}
-          />
-          <h2>HACKAUT</h2>
-        </NavLink>
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-
-
-          <div className="md:hidden">
-            <button
-              type="button"
-              className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
-              onClick={() => {
-                props.setNav(!props.nav);
-              }}
-            >
-              {props.nav ? (
-                <FaTimes size={15}></FaTimes>
-              ) : (
-                <FaBars size={15}></FaBars>
-              )}
-            </button>
-          </div>
-
-          {props.nav && (
-            <div className="w-full md:block md:w-auto" id="navbar-default">
-              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-200 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-                {links.map((link) => {
-                  return (
-                    <li key={link.id} onClick={() => { props.setNav(!props.nav) }}>
-                      <NavLink
-                        to={link.path} // Use Link instead of <a> and remove smooth and duration attributes
-                        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                        aria-current="page"
-                      >
-                        {link.link}
-                      </NavLink>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          )}
+    <header className="flex flex-wrap items-center justify-between w-full bg-blue-200 text-sm py-4 border-b-2 border-blue-700 sticky top-0 z-50">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 flex items-center justify-between">
+        <div className="flex items-center">
+          <NavLink to="/" className="flex items-center text-xl font-black gap-2">
+            <img
+              className="logonav"
+              src="src/assets/Group 2.png"
+              alt="Logo"
+              style={{ width: '25px', height: 'auto' }}
+            />
+            <h2>HACKAUT</h2>
+          </NavLink>
         </div>
+        <div className="flex items-center md:hidden">
+          <button
+            type="button"
+            className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
+            onClick={() => {
+              props.setNav(!props.nav);
+            }}
+          >
+            {props.nav ? (
+              <FaTimes size={15}></FaTimes>
+            ) : (
+              <FaBars size={15}></FaBars>
+            )}
+          </button>
+        </div>
+        {props.nav && (
+          <div className="w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-200 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+              {links.map((link) => {
+                return (
+                  <li key={link.id} onClick={() => { props.setNav(!props.nav) }}>
+                    <NavLink
+                      to={link.path}
+                      className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                      aria-current="page"
+                    >
+                      {link.link}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
         <div
           id="navbar-collapse-with-animation"
           className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
         >
-          <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end md:mt-0 sm:pl-5">
+          <div className="flex flex-col gap-2 mt-5 sm:flex-row sm:items-center sm:justify-end md:mt-0 sm:pl-5">
             {links.map((link) => {
               return (
-                <NavLink // Use Link instead of <a> and remove smooth and duration attributes
+                <NavLink
                   key={link.id}
                   to={link.path}
                   className="font-bold text-blue-900 hover:text-blue-400"
