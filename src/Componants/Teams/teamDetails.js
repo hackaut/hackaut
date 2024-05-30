2cdff87738d66763e948a84f93649d3a940addf9
Arnab Pal <arnabpalofficial2003@gmail.com>
2024-05-30 15:50:46 +0530

    fixed modular images

--- src/Componants/Teams/teamDetails.js
+++ src/Componants/Teams/teamDetails.js
@@ -1,37 +1,44 @@
+import shreyo from "../../assets/team_images/shreyoPaul.png"
+import ayan from "../../assets/team_images/Ayan.png"
+import joyjit from "../../assets/team_images/Joyjit.png"
+import mohit from "../../assets/team_images/MohitRajParit.png"
+import snehasis from "../../assets/team_images/Snehasis.png"
+import subha from "../../assets/team_images/SubhadipRoy.png"
+
 export const teamDetails = {
     shreyoPaul : {
-        name: "Sheryo Paul",
-        image:"src\\assets\\team_images\\shreyoPaul.png",
+        name: "Shreyo Paul",
+        image:shreyo,
         github:"https://github.com/ShreyoPaul",
         linkedin: "https://www.linkedin.com/in/shreyo-paul?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     },
     AyanPatra : {
         name: "Ayan Patra",
-        image:"src\\assets\\team_images\\Ayan.png",
+        image:ayan, 
         github:"https://github.com/Ayan-09",
         linkedin: "https://www.linkedin.com/in/ayanpatra09/",
     },
     joyjit : {
         name: "Joyjit Das",
-        image:"src\\assets\\team_images\\Joyjit.png",
+        image:joyjit,
         github:"https://github.com/joyjitdas3918",
         linkedin: "https://www.linkedin.com/in/joyjit-das-a380a1207?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     },
     mohitRaj : {
         name: "Mohit Raj Parit",
-        image:"src\\assets\\team_images\\MohitRajParit.png",
+        image:mohit,
         github:"https://github.com/mohitrajparit",
         linkedin: "https://www.linkedin.com/in/mohit-raj-parit-04a3411b4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     },
     snehasis : {
         name: "Snehasis Sardar",
-        image:"src\\assets\\team_images\\Snehasis.png",
+        image:snehasis,
         github:"https://github.com/SnehasisSardar",
         linkedin: "https://www.linkedin.com/in/snehasis-sardar-a9b20326b",
     },
     subhadip : {
         name: "Subhadip Roy",
-        image:"src\\assets\\team_images\\SubhadipRoy.png",
+        image:subha,
         github:"https://github.com/SubhadipSR",
         linkedin: "https://www.linkedin.com/in/subhadip-roy-sr?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     },
