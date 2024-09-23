b82fe4e5cce5aee4d432c4c89bfdaab025d67462
jnsougata <jnsougata@gmail.com>
2024-09-23 22:05:38 +0530

    Refactor CSS class name in App.css and Error.jsx

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -10,10 +10,10 @@ const Error = () => {
           <div className="col-sm-12 ">
             <div className="col-sm-10 col-sm-offset-1  text-center">
               <div className="four_zero_four_bg">
-                <img className="low" src={ErrorMsg}/>
+                <img className="low" src={ErrorMsg} alt="404" />
               </div>
 
-              <div className="contant_box_404">
+              <div className="content_box_404">
                 <h3 className="h2">Looks like you&apos;re lost</h3>
                 <p>The page you are looking for is not available!</p>
                 <NavLink to="/" className="link_404 transition duration-200 bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
