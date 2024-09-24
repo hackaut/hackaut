02360afa1aa94f2b4d44467c7e8d408b41f46320
hawkaii <parthibmukherjee@gmail.com>
2024-09-25 00:54:58 +0530

    gallery update 24

--- src/assets/image.js
+++ src/assets/image.js
@@ -1,35 +1,101 @@
-import image1 from './event/1.jpg';
-import image2 from './event/2.jpg';
-import image3 from './event/3.jpg';
-import image4 from './event/4.jpg';
-import image5 from './event/5.jpg';
-import image6 from './event/6.jpg';
+import image1 from "./event/1.jpg";
+import image2 from "./event/2.jpg";
+import image3 from "./event/3.jpg";
+import image4 from "./event/4.jpg";
+import image5 from "./event/5.jpg";
+import image6 from "./event/6.jpg";
+
+import image7 from "./event/event24_1.jpeg";
+import image8 from "./event/event24_2.jpeg";
+import image9 from "./event/event24_3.jpeg";
+import image12 from "./event/event24_5.jpeg";
+import image13 from "./event/event24_7.jpeg";
+import image14 from "./event/event24_9.jpeg";
+import image15 from "./event/event24_11.jpeg";
+import image16 from "./event/event24_12.jpeg";
+import image17 from "./event/event24_13.jpeg";
+import image18 from "./event/event24_14.jpeg";
+import image19 from "./event/event24_15.jpeg";
+import image20 from "./event/event24_16.jpeg";
+import image21 from "./event/event24_19.jpeg";
+
 const images = [
-    {
-        original: image1,
-        thumbnail: image1,
-      },
-      {
-        original: image2,
-        thumbnail: image2,
-      },
-      {
-        original: image3,
-        thumbnail: image3,
-      },
-      {
-        original: image4,
-        thumbnail: image4,
-      },
-      {
-        original: image5,
-        thumbnail: image5,
-      },
-      {
-        original: image6,
-        thumbnail: image6,
-      },
+  {
+    original: image7,
+    thumbnail: image7,
+  },
+  {
+    original: image9,
+    thumbnail: image9,
+  },
+  {
+    original: image8,
+    thumbnail: image8,
+  },
+  {
+    original: image12,
+    thumbnail: image12,
+  },
+  {
+    original: image13,
+    thumbnail: image13,
+  },
+  {
+    original: image14,
+    thumbnail: image14,
+  },
+  {
+    original: image15,
+    thumbnail: image15,
+  },
+  {
+    original: image16,
+    thumbnail: image16,
+  },
+  {
+    original: image17,
+    thumbnail: image17,
+  },
+  {
+    original: image18,
+    thumbnail: image18,
+  },
+  {
+    original: image19,
+    thumbnail: image19,
+  },
+  {
+    original: image20,
+    thumbnail: image20,
+  },
+  {
+    original: image21,
+    thumbnail: image21,
+  },
+  {
+    original: image1,
+    thumbnail: image1,
+  },
+  {
+    original: image2,
+    thumbnail: image2,
+  },
+  {
+    original: image3,
+    thumbnail: image3,
+  },
+  {
+    original: image4,
+    thumbnail: image4,
+  },
+  {
+    original: image5,
+    thumbnail: image5,
+  },
+  {
+    original: image6,
+    thumbnail: image6,
+  },
 ];
 
 export default images;
-
